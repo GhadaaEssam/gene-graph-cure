3d70c31dce1f39591a019278586e8d1650f89ac0
GhadaaEssam <CDS.GhadaEssam23243@alexu.edu.eg>
2026-07-05 15:01:43 +0300

    Remove obsolete files: package.json and start_adrs.ps1

--- start_adrs.ps1
+++
@@ -1,5 +0,0 @@
-# Always run ADRS on port 8001
-# GC-PGE prediction server uses port 8000
-& d:\Downloads\GGC\Alternative_Drug\venv\Scripts\Activate.ps1
-cd D:\Downloads\GGC\Alternative_Drug
-uvicorn main:app --reload --port 8001
